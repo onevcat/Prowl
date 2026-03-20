713659188619001fb424853c135d4d084dc9a8e6
onevcat <onevcat@gmail.com>
2026-03-20 11:19:12 +0900

    Add repository snapshot startup cache

--- supacode/Support/SupacodePaths.swift
+++ supacode/Support/SupacodePaths.swift
@@ -93,6 +93,10 @@ nonisolated enum SupacodePaths {
     baseDirectory.appending(path: "settings.json", directoryHint: .notDirectory)
   }
 
+  static var repositorySnapshotURL: URL {
+    baseDirectory.appending(path: "repository-snapshot.json", directoryHint: .notDirectory)
+  }
+
   static func repositorySettingsURL(for rootURL: URL) -> URL {
     repositorySettingsDirectory(for: rootURL)
       .appending(path: "prowl.json", directoryHint: .notDirectory)
