58ae9408a078c9717614357822c359b5dabe2ea7
bubu <bubu2131@duck.com>
2026-05-25 04:17:00 +0000

    Use Nix Playwright Chrome browser

--- web/flake.nix
+++ web/flake.nix
@@ -16,20 +16,26 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        playwrightBrowsers = pkgs.playwright-driver.browsers.override {
+          withChromium = true;
+          withChromiumHeadlessShell = false;
+          withFfmpeg = false;
+          withFirefox = false;
+          withWebkit = false;
+        };
         browsers = (builtins.fromJSON (builtins.readFile "${pkgs.playwright-driver}/browsers.json")).browsers;
         chromiumRevision = (builtins.head (builtins.filter (browser: browser.name == "chromium") browsers)).revision;
-        chromiumExecutable =
-          "${pkgs.playwright-driver.browsers}/chromium-${chromiumRevision}/chrome-linux64/chrome";
+        chromiumExecutable = "${playwrightBrowsers}/chromium-${chromiumRevision}/chrome-linux64/chrome";
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             bun
-            playwright-driver.browsers
+            playwrightBrowsers
           ];
 
           shellHook = ''
-            export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
+            export PLAYWRIGHT_BROWSERS_PATH="${playwrightBrowsers}"
             export PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH="${chromiumExecutable}"
             export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
             export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
